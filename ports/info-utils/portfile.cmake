d8b412c06a9689c5460bff2e1046958e9a570f20
bodand <bodand@proton.me>
2023-01-12 18:11:10 +0100

    fix: InfoUtils package unavailable pt.9.1

--- ports/info-utils/portfile.cmake
+++ ports/info-utils/portfile.cmake
@@ -4,7 +4,10 @@ vcpkg_from_github(
     REF "ebfdde12654d6d75d5518e9e64d26c85bd002005"
     SHA512 "191685c9c0a25d91c38012f13964924de718099d6f92ecc464e776313e1348a520ad9ee5a6ca3607ab54c9ba5521990d7beaa3648e1ca832288411e77b9e017d"
     PATCHES
-        "00-cmake-include-fix.patch")
+        "00-cmake-include-fix.patch"
+        "01-cmake-install-fix.patch"
+        "02-cmake-disable-pseudo-pkgmgr.patch" # this wasn't doing anything since tests are disabled, but just to make sure
+        )
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}"
                       OPTIONS
@@ -12,6 +15,9 @@ vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}"
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME "InfoUtils"
                          CONFIG_PATH "lib/cmake/InfoUtils")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" 
      DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
