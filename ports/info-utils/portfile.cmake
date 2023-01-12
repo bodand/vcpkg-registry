8028384a5054d28b80a20c9d6f8758056a60bcf8
bodand <bodand@proton.me>
2023-01-12 17:31:16 +0100

    fix: InfoUtils package unavailable pt.7.1

--- ports/info-utils/portfile.cmake
+++ ports/info-utils/portfile.cmake
@@ -6,10 +6,14 @@ vcpkg_from_github(
     PATCHES
         "00-cmake-include-fix.patch")
 
-vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS
-        "-DInfoUtils_BUILD_TESTS=OFF")
+vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}"
+                      OPTIONS
+                          "-DInfoUtils_BUILD_TESTS=OFF")
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(PACKAGE_NAME "InfoUtils")
+vcpkg_cmake_config_fixup(PACKAGE_NAME "InfoUtils"
+                         CONFIG_PATH "lib/cmake/InfoUtils")
+
+file(INSTALL "${SOURCE_PATH}/LICENSE.adoc" 
+     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+     RENAME copyright)
 
